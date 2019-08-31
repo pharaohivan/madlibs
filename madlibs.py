c53d7d9308c29ec970fd6f2da0031282a2c838d8
Ivan <ivanhallcloninger@gmail.com>
2019-08-30 19:13:38 -0700

    second commit

--- madlibs.py
+++ madlibs.py
@@ -3,4 +3,5 @@ noun = input("Enter a noun")
 verb = input("Enter a verb")
 pronoun = input("Enter a pronoun")
 
-print("I was walking" + adjective + "when I was beamed to + noun + .")
+print("I was walking " + adjective + " when I was beamed to " + noun + ".")
+print("That's when I started" + verb + ".")
