782be3ee4e76edd100143112109a6f45799181a7
Ivan <ivanhallcloninger@gmail.com>
2019-08-30 18:55:24 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+adjective = input("Enter a adjective")
+noun = input("Enter a noun")
+verb = input("Enter a verb")
+pronoun = input("Enter a pronoun")
+
+print("I was walking" + adjective + "when I was beamed to + noun + .")
