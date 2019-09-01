04600a4e1d60542c6c537f7453e44eddd61c7011
Ivan <ivanhallcloninger@gmail.com>
2019-09-01 00:29:04 -0700

    third commit

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,12 @@
-adjective = input("Enter a adjective")
-noun = input("Enter a noun")
+print("Enter 3 adjectives")
+adjective = [input(),input(),input()]
+print("Enter 5 nouns")
+noun = [input(),input(),input(),input(),input()]
 verb = input("Enter a verb")
 pronoun = input("Enter a pronoun")
 
-print("I was walking " + adjective + " when I was beamed to " + noun + ".")
-print("That's when I started" + verb + ".")
+print("I was walking " + adjective[0] + " when I was beamed to " + noun[0] + ".")
+print("That's when I started " + verb + ".")
+print("I was terrified, you could see the" + noun[1] + " coming from my eyes")
+print("Not only didn't I expect to see" + pronoun + ", I didn't expect to see all the" + noun[2])
+print("that came along with them. Back on earth I didnt have any " + noun[3])
