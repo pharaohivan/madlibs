61d4e1c3eb706f6ac22b7f15b3db49a8b7fbe1fd
Ivan <ivanhallcloninger@gmail.com>
2019-09-01 19:44:41 -0700

    formatused

--- madlibs.py
+++ madlibs.py
@@ -2,11 +2,16 @@ print("Enter 3 adjectives")
 adjective = [input(),input(),input()]
 print("Enter 5 nouns")
 noun = [input(),input(),input(),input(),input()]
-verb = input("Enter a verb")
-pronoun = input("Enter a pronoun")
+print("Enter 4 verbs")
+verb = [input(),input(),input(), input()]
 
-print("I was walking " + adjective[0] + " when I was beamed to " + noun[0] + ".")
-print("That's when I started " + verb + ".")
-print("I was terrified, you could see the" + noun[1] + " coming from my eyes")
-print("Not only didn't I expect to see" + pronoun + ", I didn't expect to see all the" + noun[2])
-print("that came along with them. Back on earth I didnt have any " + noun[3])
+pronoun = input("Enter a pronoun \n")
+
+print("\n\n\n\n\n\n\n\nI was walking " + adjective[0] + " when I was beamed to a " + noun[0] + ".")
+print("That's when I started " + verb[0] + ", I was terrified,")
+print("you could see the " + noun[1] + " coming from my eyes.")
+print("Not only didn't I expect to see " + pronoun + ", I didn't expect to see all the " + noun[2] + "s")
+print("that came along with them. Back on earth I didnt have any " + noun[3] + ".")
+print("Here, in this place, there was " + noun[4] + " everywhere. I couldn't help but " + verb[1] + ".")
+print("What even happened, I thought to myself. Suddenly, the {} {}".format( adjective[1], verb[2]))
+print("turned into {} {} and I was home again.".format ( adjective[2], verb[3]))
