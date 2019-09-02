4a2f1508252a2727d825c51cdc936f4cdcbaca5a
Ivan <ivanhallcloninger@gmail.com>
2019-09-01 21:06:43 -0700

    storyfinalfinished

--- madlibs.py
+++ madlibs.py
@@ -1,11 +1,11 @@
 
-
+print("Welcome")
 print("Enter 3 adjectives")
 adjective = [input(),input(),input()]
-print("Enter 5 nouns")
-noun = [input(),input(),input(),input(),input()]
-print("Enter 4 verbs")
-verb = [input(),input(),input(), input()]
+print("Enter 6 nouns")
+noun = [input(),input(),input(),input(),input(),input()]
+print("Enter 2 verbs")
+verb = [input(),input()]
 
 pronoun = input("Enter a pronoun \n")
 
@@ -14,6 +14,6 @@ print("That's when I started to " + verb[0] + ", I was terrified,")
 print("you could see the " + noun[1] + " coming right after me.")
 print("Not only didn't I expect to see the {}, I didn't expect to see all of the {}s".format(noun[2], noun[3]))
 print("that came along with them. Back on earth I didn't have any " + noun[4] + "s.")
-print("Here, in this place, there were " + noun[4] + "'s everywhere. I couldn't help but " + verb[1] + ".")
-print("What even happened? Suddenly, the {} {} turned into {} {}".format( adjective[1], verb[2], adjective[2], verb[3]))
+print("Here, in this place, there were " + noun[4] + "'s everywhere. I couldn't help but " + verb[0] + ".")
+print("What even happened? Suddenly, the {} {}s turned into {} {}s".format(adjective[1], verb[1], adjective[2], noun[5]))
 print("and I was home again. Thank goodness.")
